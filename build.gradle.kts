d3f3df04e255b2f71e482e17773f72f1b28e2650
AntipovAndrey <antipov11.11@gmail.com>
2019-02-14 16:57:16 +0300

    plugin action setup

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("org.jetbrains.intellij") version "0.4.1"
+    kotlin("jvm") version "1.3.11"
+}
+
+group = "com.github.antipovandrey"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    compile(kotlin("stdlib-jdk8"))
+    testCompile("junit", "junit", "4.12")
+}
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
---
+++ src/main/kotlin/com/github/antipovandrey/generator/GenerateModuleAction.kt
@@ -0,0 +1,12 @@
+package com.github.antipovandrey.generator
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.project.DumbAware
+
+class GenerateModuleAction : AnAction(), DumbAware {
+
+    override fun actionPerformed(e: AnActionEvent) {
+
+    }
+}
